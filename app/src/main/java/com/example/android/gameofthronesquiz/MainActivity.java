a05ea9e3c96537b5b138ff42b8e70fdef6e1297c
Donald Mears <DBmears@fullsail.edu>
2018-05-21 19:40:34 -0400

    Added methods in main for Questions 1 and 2.

--- app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
+++ app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
@@ -3,17 +3,53 @@ package com.example.android.gameofthronesquiz;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int points = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView name = (TextView) findViewById(R.id.quizName);
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/Game of Thrones.ttf");
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Game of Thrones.ttf");
         name.setTypeface(font);
+
+
+    }
+
+    public void radioQuestionOne(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.radio_westoros:
+                if (checked) {
+                    points += 1;
+                }
+                break;
+            default:
+                points += 0;
+        }
+    }
+
+    public void radioQuestionTwo(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.radio_wife:
+                if (checked) {
+                    points += 1;
+                }
+            default:
+                points += 0;
+        }
     }
 }
+
+
