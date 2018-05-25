09a2d8a7c35413124c942b20cfe1dec04d697c22
Donald Mears <DBmears@fullsail.edu>
2018-05-25 18:24:46 -0400

    Updated MainActivity.java. Working as intended.

--- app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
+++ app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
@@ -4,13 +4,17 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import javax.microedition.khronos.egl.EGLDisplay;
+
 public class MainActivity extends AppCompatActivity {
 
+    int score;
 
 
     @Override
@@ -19,9 +23,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView name = (TextView) findViewById(R.id.quizName);
+        TextView textName = (TextView) findViewById(R.id.quizName);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Game of Thrones.ttf");
-        name.setTypeface(font);
+        textName.setTypeface(font);
+
+    }
+
+
+    public int calculateScore(boolean one, boolean two, boolean three, boolean four, boolean five, boolean six) {
+        int runningScore = 0;
+
+        if (one) {
+            runningScore += 1;
+        }
+        if (two) {
+            runningScore += 1;
+        }
+        if (three) {
+            runningScore += 1;
+        }
+        if (four) {
+            runningScore += 1;
+        }
+        if (five) {
+            runningScore += 1;
+        }
+        if (six) {
+            runningScore += 1;
+        }
+        return runningScore;
+    }
+
+    public void submitQuiz(View view) {
+
+        EditText userName = findViewById(R.id.name_input_view);
+        String name = userName.getText().toString();
 
         RadioButton oneRadioButton = findViewById(R.id.radio_westoros);
         boolean questionOne = oneRadioButton.isChecked();
@@ -39,20 +75,19 @@ public class MainActivity extends AppCompatActivity {
         boolean questionFive = fiveRadioButton.isChecked();
 
         RadioButton sixRadioButton = findViewById(R.id.radio_dragon2);
-        boolean questionSix = fourRadioButton.isChecked();
+        boolean questionSix = sixRadioButton.isChecked();
 
-        int score = calculateScore(questionOne,questionTwo,questionThree,questionsFour,questionFive,questionSix);
+        score = calculateScore(questionOne, questionTwo, questionThree, questionsFour, questionFive, questionSix);
 
-
+        printResults(name);
     }
-
-
-    public int calculateScore(boolean one, boolean two, boolean three, boolean four, boolean five, boolean six) {
-        if (points == 6) {
-            /*
-            String resultsScore = "You scored " + points + " out of 6!";
-            Toast.makeText(this, resultsScore, Toast.LENGTH_LONG).show();
-            */
+    public void printResults(String name){
+        if (score <= 2) {
+            Toast.makeText(this, name+" you scored "+score+" points. You may need to watch some more GoT!", Toast.LENGTH_SHORT).show();
+        } else if (score <= 4) {
+            Toast.makeText(this, name+" you scored "+score+" points. You are a friend of the crown!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, name+" you scored "+score+" points. DonalYou are a true lord of the realm!", Toast.LENGTH_SHORT).show();
         }
     }
 }
